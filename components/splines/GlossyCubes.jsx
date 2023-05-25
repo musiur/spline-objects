a02958c0eb958d19d41c740eb5e9fbe165e7b41e
musiur <musiur.opu@gmail.com>
2023-05-26 05:18:38 +0600

    first commit

---
+++ components/splines/GlossyCubes.jsx
@@ -0,0 +1,11 @@
+
+import Spline from "@splinetool/react-spline";
+
+export default function GlossyCubes() {
+  return (
+    <Spline
+      scene="https://prod.spline.design/KyO9gKkFWmLVjtR4/scene.splinecode"
+      style={{ width: "100%", height: "100%" }}
+    />
+  );
+}
